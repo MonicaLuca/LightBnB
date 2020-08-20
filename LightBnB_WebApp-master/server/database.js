134cab0b3d80ffed7024f11faebc6579d14de73d
MonicaLuca <monica.lucarini28@gmail.com>
2020-08-20 13:39:43 -0400

    LighthouseBnB Reservations modify function to use SQL queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -20,9 +20,9 @@ const getUserWithEmail = function(email) {
   return pool.query(`
   SELECT *
   FROM users
-  WHERE email = $1`, [email])
+  WHERE email = $1;`, [email])
   .then(res => res.rows[0]);
-}
+};
 
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -36,9 +36,9 @@ const getUserWithId = function(id) {
 return pool.query(`
 SELECT *
 FROM users
-WHERE id = $1`, [id])
+WHERE id = $1;`, [id])
 .then(res => res.rows[0]);
-}
+};
 
 exports.getUserWithId = getUserWithId;
 
@@ -53,7 +53,7 @@ const addUser = function (user) {
   return pool.query(
   `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password])
   .then(res => res.rows[0])
-}
+};
 
 exports.addUser = addUser;
 
@@ -64,9 +64,22 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
+
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  return pool.query(
+  `SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+  .then(res => res.rows)
+};
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
@@ -81,19 +94,11 @@ exports.getAllReservations = getAllReservations;
 const getAllProperties = function(options, limit = 10) {
   return pool.query(`
   SELECT * FROM properties
-  LIMIT $1
+  LIMIT $1;
   `, [limit])
   .then(res => res.rows);
-}
+};
 
-// ORIGINAL
-// const getAllProperties = function(options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// }
 exports.getAllProperties = getAllProperties;
 
 
